ffb56c665ae9adf1393905d4ebe8621af5cab7e7
Brian Reid <brian.reid@ibm.com>
2020-09-02 08:40:18 -0400

    take args from the user

--- evenOddPartition.py
+++ evenOddPartition.py
@@ -1,4 +1,14 @@
 import random
+import argparse
+
+def createArray(min, max, size):
+    print("Initializing the input array...\nMax integer value: {}\nMin integer value: {}\nArray size: {}".format(max, min, size))
+    A = []
+    for i in range(0, size):
+        n = random.randint(min, max)
+        A.append(n)
+    print("Input array: {}".format(A))
+    return A
 
 def swapPositions(list, pos1, pos2): 
       
@@ -6,19 +16,28 @@ def swapPositions(list, pos1, pos2):
     return list
 
 def partitionEvenOdd(A, n):
+    print("Sorting the array...")
     lastEven = -1
     for i in range(0, n):
         if A[i] % 2 == 0:
             if lastEven != (i-1):
                 swapPositions(A, i, lastEven + 1)
             lastEven = lastEven + 1
+    print("Sort complete.\nPartitioned array: {}".format(A))
 
-# A = [2,2,1,2,1,2,1,2,1]
 
-A = []
-for i in range(0,25):
-    n = random.randint(1,100)
-    A.append(n)
-print(A)
-partitionEvenOdd(A, len(A))
-print(A)
+if __name__ == "__main__":
+    # initialize a parser
+    parser = argparse.ArgumentParser()
+
+    # add args to the parser
+    parser.add_argument("-m", "--min", type=int, help="minimum value allowed for the integer array")
+    parser.add_argument("-M", "--max", type=int, help="maximum value allowed for the integer array")
+    parser.add_argument("-s", "--sample-size", dest="size", type=int, help="size of the input array")
+
+    # parse the arguments
+    args = parser.parse_args()
+
+    # create and partition the array
+    A = createArray(args.min, args.max, args.size)
+    partitionEvenOdd(A, len(A))
